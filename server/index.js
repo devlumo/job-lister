28bc22044940a8d2ae869c7c9ec8785be7e1dc2c
Luke Molloy <developerlumo@gmail.com>
2021-11-03 15:47:55 +0000

    Added mongoose and connections to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import "./config.js";
 import app from "./app.js";
 
+import mongoose from "mongoose";
+
+// CONNECT TO MONGODB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Database connected");
+});
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
