aee99cfcedd204350cd4b86450dadb2bb9b69890
Luke Molloy <developerlumo@gmail.com>
2021-11-03 17:09:43 +0000

    Added react App component useEffect to request from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
+  // use loading state to avoid error while waiting on api request
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get("http://127.0.0.1:3001/");
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>Hello World</h1>
+      <div>{loading ? "loading" : data.status}</div>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 if (process.env.WORKING_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
