2b3b3ef528f46b5478293b43ca7f2253b17f1026
Luke Molloy <developerlumo@gmail.com>
2021-11-04 15:57:21 +0000

    installed bcrypt and hashed passwords prior to saving new user in the db via middleware

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bycrpt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
     required: [true, "Email is required"],
     validate: [validator.isEmail, "Please provide a valid email address"],
     maxlength: [32, "Email is too long"],
@@ -14,12 +16,22 @@ const userSchema = new mongoose.Schema({
   },
   userName: {
     type: String,
+    unique: true,
     minlength: [3, "Username must be 3 characters or longer"],
   },
   dateCreated: Date,
 });
 
 // MIDDLEWARES
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bycrpt.hash(this.password, 10);
+  next();
+});
 
 userSchema.pre("save", function (next) {
   this.dateCreated = Date.now();
